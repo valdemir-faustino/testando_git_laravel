24674b0da15ac9af1055c3b897626467f335e389
Valdemir-Faustino <valfaustino2@hotmail.com>
2025-09-10 22:14:05 -0300

    Complementação das rotas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use App\Http\Controllers\GitController;
 
 
 Route::get('/',[GitController::class, "index"])->name("index");
+
+Route::get('/create',[GitController::class, "create"])->name("create");
+
+Route::post('/store',[GitController::class, "store"])->name("store");
+
+Route::get('/show',[GitController::class, "show"])->name("show");
+
+Route::get('/edit',[GitController::class, "edit"])->name("edit");
+
+Route::put('/update',[GitController::class, "update"])->name("update");
+
+Route::delete('/destroy',[GitController::class, "destroy"])->name("destroy");
